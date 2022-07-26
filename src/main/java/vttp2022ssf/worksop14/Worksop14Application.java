2ec49628da02fe046e1e10687140dd1cc9c79e61
LimWaiLeongJeremy <jereremy19995@hotmail.sg>
2022-07-26 09:50:51 +0800

    first commit

---
+++ src/main/java/vttp2022ssf/worksop14/Worksop14Application.java
@@ -0,0 +1,13 @@
+package vttp2022ssf.worksop14;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Worksop14Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Worksop14Application.class, args);
+	}
+
+}
---
+++ src/test/java/vttp2022ssf/worksop14/Worksop14ApplicationTests.java
@@ -0,0 +1,13 @@
+package vttp2022ssf.worksop14;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Worksop14ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
